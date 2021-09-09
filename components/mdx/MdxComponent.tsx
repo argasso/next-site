d3ddb748e18ed6217304afe386d73cdbf68ad088
Robert Olsson <robert.olsson@argasso.se>
2021-09-09 20:45:58 +0200

    Without getMdxComponent

--- components/mdx/MdxComponent.tsx
+++ components/mdx/MdxComponent.tsx
@@ -1,20 +1,20 @@
-import { H1, H2, H3, UL, Blockquote } from '../Html'
-import { KatalogForm } from './components/KatalogForm'
-import { getMDXComponent } from 'mdx-bundler/client'
-import { useMemo } from 'react'
+// import { H1, H2, H3, UL, Blockquote } from '../Html'
+// import { KatalogForm } from './components/KatalogForm'
+// import { getMDXComponent } from 'mdx-bundler/client'
+// import { useMemo } from 'react'
 // import * as UI from './components'
 
-const components = {
-  H1,
-  H2,
-  H3,
-  h1: H1,
-  h2: H2,
-  h3: H3,
-  ul: UL,
-  blockquote: Blockquote,
-  KatalogForm,
-}
+// const components = {
+//   H1,
+//   H2,
+//   H3,
+//   h1: H1,
+//   h2: H2,
+//   h3: H3,
+//   ul: UL,
+//   blockquote: Blockquote,
+//   KatalogForm,
+// }
 
 interface Props {
   code: string
@@ -22,10 +22,11 @@ interface Props {
 
 export function MdxComponent({ code }: Props) {
   // console.log(code)
-  const Component = useMemo(() => getMDXComponent(code), [code])
+  // const Component = useMemo(() => getMDXComponent(code), [code])
   return (
     <div className="wrapper">
-      <Component components={components} />
+      {code}
+      {/* <Component components={components} /> */}
     </div>
   )
 }
